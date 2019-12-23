4259e0c24fcc9d3425cdbeb28732eb63f37e80c2
Morgan Pierson <morganpierson@gmail.com>
2019-12-22 21:02:01 -0800

    basic api request successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import UserCard from "./UserCard";
 
+const API_KEY = "f7322ae2ae53159d961a474b67516e";
+
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://uifaces.co/api?limit=10&emotion[]=happiness", {
+      method: "GET",
+      headers: {
+        "X-API-KEY": API_KEY,
+        Accept: "application/json",
+        "Cache-Control": "no-cache"
+      }
+    })
+      .then(res => res.json())
+      .then(response => console.log(response));
+  });
+
   return (
-    <UserCard name={"Morgan"} pic={"https://www.placecage.com/c/200/300"} />
+    <div>
+      <UserCard name={"Morgan"} pic={"https://www.placecage.com/c/200/300"} />
+      <UserCard name={"Nick"} pic={"https://www.placecage.com/c/200/300"} />
+      <UserCard
+        name={"Crazy man"}
+        pic={"https://www.placecage.com/c/200/300"}
+      />
+    </div>
   );
 };
 
--- src/UserCard.js
+++ src/UserCard.js
@@ -1,9 +1,19 @@
-import React from "react";
-export default function Pet({ name, pic }) {
+import React, { useState } from "react";
+
+const UserCard = ({ pic }) => {
+  const [name, setName] = useState("Nick");
   return (
     <div>
       <img src={pic} alt="User avatar" />
+      <input
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
       {name}
     </div>
   );
-}
+};
+
+export default UserCard;
